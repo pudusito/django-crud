6f7ea102e0e1b77d89f9354ddc77c768cae277b8
pudusito <benjaminmaldonadobarrales@gmail.com>
2025-09-30 06:52:37 -0300

    act

--- project/settings.py
+++ project/settings.py
@@ -27,15 +27,20 @@ try:
 except Exception:
     pass
 
+# Usar PyMySQL
+if os.getenv("USE_PYMYSQL", "0") == "1":
+    import pymysql
+    pymysql.install_as_MySQLdb()
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0r99ahp!r=$cxl9#opoiwyv$t2x=)y^v_^6*31t)$y7834w#j+'
+SECRET_KEY = os.getenv("SECRET_KEY", "inseguro_para_dev")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG","True")== "True"
 
 ALLOWED_HOSTS = []
 
@@ -85,15 +90,23 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-# Usar PyMySQL
-if os.getenv("USE_PYMYSQL", "0") == "1":
- import pymysql
- pymysql.install_as_MySQLdb()
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": os.getenv("MYSQL_DATABASE"),  # Use MYSQL_DATABASE
+        "USER": os.getenv("MYSQL_USER"),      # Use MYSQL_USER
+        "PASSWORD": os.getenv("MYSQL_PASSWORD"),  # Use MYSQL_PASSWORD
+        "HOST": os.getenv("MYSQL_HOST"),      # Use MYSQL_HOST
+        "PORT": int(os.getenv("MYSQL_PORT", "3306")),  # Convert to integer
+        "OPTIONS": {
+            "charset": "utf8mb4",
+            "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
+        **(
+            {"ssl": {"ca": os.getenv("MYSQL_SSL_CA")}}
+            if os.getenv("MYSQL_SSL_CA")
+            else {}
+            ),    
+        },
     }
 }
 
